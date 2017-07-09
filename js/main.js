68239a13a360b57f52907d33d6301daff2521b91
Amidou Kante <kamdiou@gmail.com>
2017-07-09 15:41:30 -0400

    capable of changing the board and logging a winner

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const firstVal = /[0 3 6]/g;
 const middleVal = /[1 4 7]/g;
 const finalVal = /[2 5 8]/g;
-
-const colVal1 = /[0 1 2]/g;
-const colVal2 = /[3 4 5]/g;
-const colVal3 = /[6 7 8]/g;
+const row1 = /[0 1 2]/g;
+const row2 = /[3 4 5]/g;
+const row3 = /[6 7 8]/g;
+const leftToRight = /[0 4 8]/g;
+const rightToLeft = /[2 4 6]/g;
 
 let counter = 0;
 let gameBoard = []
@@ -29,27 +30,33 @@ function addClicks(){
 function clickEvent(event){
     let id = event.target.id;
     let item = document.getElementById(id);
-    console.log(`Tile ${id} was clicked`);
 
-    //This will log a 'x' on the board
-    if(counter%2 === 0 && counter !== 9){
-        counter++;
-        item.innerHTML += (`<p>x</p>`);
-        gameBoard[id] = 'x';
-        console.log(gameBoard);
-        checkScore(id);
-    }//This will log a 'o' on the board
-    else if(counter%2 !== 0 && counter !== 9){
-        counter++;
-        item.innerHTML += (`<p>o</p>`);
-        gameBoard[id] = 'o';
-        console.log(gameBoard);
-        checkScore(id);
-    }//Ends the game if board gets full 
-    if(counter === 9){
-        console.log('No more moves available');
-        counter = 0;
-        // location.reload()
+    if(event.target.className === "tile"){
+        console.log(`Tile ${event.target.className} was clicked`);
+        //This will log a 'x' on the board
+        if(counter%2 === 0 && counter !== 9){
+            counter++;
+            item.innerHTML += (`<p>x</p>`);
+            item.className = "tile clicked";
+            gameBoard[id] = 'x';
+            console.log(gameBoard);
+            checkScore(id);
+        }//This will log a 'o' on the board
+        else if(counter%2 !== 0 && counter !== 9){
+            counter++;
+            item.innerHTML += (`<p>o</p>`);
+            item.className = "tile clicked";
+            gameBoard[id] = 'o';
+            console.log(gameBoard);
+            checkScore(id);
+        }//Ends the game if board gets full 
+        if(counter === 9){
+            console.log('No more moves available');
+            counter = 0;
+            // location.reload()
+        }
+    }else {
+        console.log("Choose another tile")
     }
 }
 
@@ -60,6 +67,12 @@ function checkScore(id){
         if(gameBoard[id]==='x'){
             console.log('Player 1 won')
         }else if(gameBoard[id]==='o'){
+            console.log('Player 2 won')
+        }
+       }else if(gameBoard[id] === gameBoard[3] && gameBoard[3] === gameBoard[6]){
+        if(gameBoard[id]==='x'){
+            console.log('Player 1 won')
+        }else{
             console.log('Player 2 won')
         }
        }
